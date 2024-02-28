b8e2337ca49adc1ace42ec8bdfb48cfc17af3b31
Tom Gluth <156343871+glutto-3001@users.noreply.github.com>
2024-02-28 21:12:36 +0100

    defined common variables and symbols of the game

---
+++ schiffe_versenken.py
@@ -0,0 +1,16 @@
+# symbols of the game
+
+#       X = hit
+#       O = miss
+#       # = ship
+#   space = water/placeholder
+
+# common variables
+field_1 = []
+field_2 = []
+shotfield_1 = []
+shotfield_2 = []
+victory = False
+active_player = 0
+shot = False
+hit = False

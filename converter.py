31ff1be168ce2c71d39c8ef839dbe9099a7f21f5
Edgar Malinowsky <30591377+UnknownEdgar@users.noreply.github.com>
2024-02-29 17:10:54 +0100

    added error protection

--- converter.py
+++ converter.py
@@ -1,7 +1,54 @@
 def grid_conversion():
-    line = int(input("Zeile: "))
-    row = int(input("Spalte: "))
 
-    field = row - 1 + ((line - 1) * 5)
+    keep_going = 0
+    
+        
+    while keep_going != 1:
+        
+        keep_going = 1
+    
+        try:
+            row = str(input("Spalte: "))
+        except ValueError:
+            error = True
+    
+        if (row == "A") or (row == "a"):
+            i_row = 0
+
+        elif (row == "B") or (row == "b"):
+            i_row = 1
+
+        elif (row == "C") or (row == "c"):
+            i_row = 2
+
+        elif (row == "D") or (row == "d"):
+            i_row = 3
+
+        elif (row == "E") or (row == "e"):
+            i_row = 4
+
+        else:
+            print("Falsche Eingabe (A - E)")
+            keep_going = 0
+
+    keep_going = 0
+
+    while keep_going != 1:
+
+        keep_going = 1
+
+        try:
+            line = int(input("Zeile: "))
+        except ValueError:
+            error = True
+        
+        if 0 < line < 6:
+            line = line
+        else:
+            keep_going = 0
+            print("Faslche Eingabe (1 - 5)")
+
+
+    field = i_row + ((line - 1) * 5)
 
     return field

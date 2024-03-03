8a47555f1f51ba36dd9581fdbfb1414d74148601
Tom Gluth <156343871+glutto-3001@users.noreply.github.com>
2024-03-03 15:38:08 +0100

    removed "magic numbers"

--- ausgabe.py
+++ ausgabe.py
@@ -1,12 +1,12 @@
-def print_field(in_array):
-    ausgabe = "  A B C D E"
+def print_field(in_array, GRID):
+    ausgabe = "  A B C D E"                                                             # generate the output of fields in terminal
     for i in range(25):
 
-        if (i % 5) != 0:
+        if (i % GRID) != 0:
             ausgabe = ausgabe + in_array[i] + "|"
         
         else:
-            line = int(i / 5 + 1)
+            line = int(i / GRID + 1)
             ausgabe = ausgabe + "\n" + str(line) + "|" + in_array[i] + "|"
 
     print(ausgabe)

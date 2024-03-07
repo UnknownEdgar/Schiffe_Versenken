93174d6a2f9e039215a9005452fd51c28849fd29
Edgar Malinowsky <30591377+UnknownEdgar@users.noreply.github.com>
2024-03-07 17:53:56 +0100

    changed ausgabe to 10x10 grid

--- ausgabe.py
+++ ausgabe.py
@@ -1,10 +1,14 @@
 def print_field(in_array, GRID, FIELD_SIZE):
-    ausgabe = "  A B C D E F G H I J"                                                             # generate the output of fields in terminal
+    ausgabe = "   A B C D E F G H I J"                                                             # generate the output of fields in terminal
     for i in range(FIELD_SIZE):
 
         if (i % GRID) != 0:
             ausgabe = ausgabe + in_array[i] + "|"
         
+        elif i < (FIELD_SIZE - GRID -  1):
+            line = int(i / GRID + 1)
+            ausgabe = ausgabe + "\n " + str(line) + "|" + in_array[i] + "|"
+
         else:
             line = int(i / GRID + 1)
             ausgabe = ausgabe + "\n" + str(line) + "|" + in_array[i] + "|"

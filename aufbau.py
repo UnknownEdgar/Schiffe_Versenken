ff33cab24a69ddccf5dda75e15fa3aa983222e93
Edgar Malinowsky <30591377+UnknownEdgar@users.noreply.github.com>
2024-03-07 18:29:52 +0100

    added comments on aufbau

--- aufbau.py
+++ aufbau.py
@@ -5,7 +5,7 @@ import converter as cv
 # function generating placement of the ships
 def aufbau(field_player, FIELD_SIZE, GRID):
       
-      a.print_field(field_player, GRID, FIELD_SIZE)
+      a.print_field(field_player, GRID, FIELD_SIZE)   # print the playfield
 
       print("In welche Felder möchtest du deine Schiffe setzen?\nZur Verfügung stehen 1 Schlachtschiff (5 Felder), 2 Kreuzer (4 Felder), 3 Zerstörer (3 Felder), 4 U-Boote (2 Felder).\nDie Schiffe werden auch in dieser Reihenfolge (5 -> 2) platziert.\nBitte gib immer das linke obere Feld an.")
     
@@ -21,38 +21,42 @@ def aufbau(field_player, FIELD_SIZE, GRID):
             except ValueError:
                   error = True
 
-            # placement of ships with size of 5
+            # check if the user wants to reset the board
             if dir == 8:
                   
-                  # empty the field while placement
-                  
+                  # empty the field and restart the process to place the ships
                   gen_empty(field_player, FIELD_SIZE)
                   ship = 0
 
+            # continue if no reset was chosen
             else:
 
+                  # place the ship with size 5
                   if ship < 1:
                         
                         SHIP_SIZE = 5
                         ship = place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, dir, ship)
                   
-                  # placement of ships with size of 2
+                  # placement of the ships with size 4
                   elif 1 <= ship < 3:
 
                         SHIP_SIZE = 4
                         ship = place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, dir, ship)
 
+                  # placement of the ships with size 3
                   elif 3 <= ship < 6:
 
                         SHIP_SIZE = 3
                         ship = place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, dir, ship)
 
+                  # placement of the ships with size 2
                   elif 6 <= ship < 10:
 
                         SHIP_SIZE = 2
                         ship = place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, dir, ship)        
 
 
+            # print the current field after placing the ship
             a.print_field(field_player, GRID, FIELD_SIZE)
 
       # delete support characters for ship placement
@@ -62,6 +66,7 @@ def aufbau(field_player, FIELD_SIZE, GRID):
 
             else:
                   field_player[i] = field_player[i]           
+      
       # show the field with placed ships
       print("Aktuelles Spielfeld")
       a.print_field(field_player, GRID, FIELD_SIZE)
@@ -83,6 +88,7 @@ def add_below(field_array, pos, field_size, grid):
       if pos < (field_size - grid):
             field_array[pos + grid] = "\033[1;30m" + "O" + "\033[00m"
 
+# combination of some functions of the generation of support characters
 def add_abr(playfield, position, loop, field_size, grid):
       add_above(playfield, position + loop, grid)
       add_below(playfield, position + loop, field_size, grid)
@@ -103,11 +109,17 @@ def gen_array(in_array,size):
       for i in range(size):
             in_array.append(" ")
 
+# function to place the ships on the playfield
 def place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, direction, ship):
       
       error_hor = False
       error_ver = False
 
+      # the ships aren't allowed to be placed in a field where
+            #     - the ship would exceed the maximum number of rows
+            #     - the ship would exceed the maximum number of lines
+            #     - a support character or boat is placed
+      
       # check for horizontal placement error
       for i in range(SHIP_SIZE):
             
@@ -125,6 +137,10 @@ def place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, direction, ship
             
             elif direction == 1 and (field_player[field + (i * GRID)] == "\033[1;30m" + "O" + "\033[00m" or field_player[field + (i * GRID)] == "\033[32m" + "#" + "\33[00m"):
                   error_ver = True
+
+      # placement of the ships
+      #     add a boat character to every field where it should be
+      #     add support characters to all surrounding fields
 
       # horizontal placement of ship
       if direction == 0 and (field % GRID) < (GRID - SHIP_SIZE + 1) and error_hor == False:                        
@@ -145,4 +161,5 @@ def place_ship(field, field_player, FIELD_SIZE, SHIP_SIZE, GRID, direction, ship
       else:
             print("Falsche Eingabe. Bitte erneut probieren")
 
+      # return the integer ship to get to the next ship or repeat the process with the same ship
       return ship
